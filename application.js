cbe8be677c60ce358a9551cfa007093524a27d09
Mike Axtman <mdaxtman@gmail.com>
2015-06-01 20:20:52 -0700

    unified search with api

--- application.js
+++ application.js
@@ -1,11 +1,12 @@
-(function($){
+
+var getRequest = function(val) {
 
 
-	var phrase = 'united states';
+	var phrase = val,
 		key = '3ed40dc640c04223adba52fddb235425',
 		capitolWordsAPI = 'http://capitolwords.org/api/1/dates.json',
 		startDate = '1995',
-		granularity = 'years';
+		granularity = 'year';
 
 	phrase = phrase.replace(/\s/, '+');
 
@@ -17,9 +18,48 @@
 
 	$.get(requestURI, function( data ) {
 		console.log(data.results);
+
+
+
+
+
+
+
+        /*
+google.load('visualization', '1', {packages: ['corechart', 'bar']});
+google.setOnLoadCallback(drawMaterial);
+
+function drawMaterial() {
+      var data = google.visualization.arrayToDataTable([
+        ['City', '2010 Population', '2000 Population'],
+        ['New York City, NY', 8175000, 8008000],
+        ['Los Angeles, CA', 3792000, 3694000],
+        ['Chicago, IL', 2695000, 2896000],
+        ['Houston, TX', 2099000, 1953000],
+        ['Philadelphia, PA', 1526000, 1517000]
+      ]);
+
+      var options = {
+        chart: {
+          title: 'Population of Largest U.S. Cities'
+        },
+        hAxis: {
+          title: 'Total Population',
+          minValue: 0,
+        },
+        vAxis: {
+          title: 'City'
+        },
+        bars: 'horizontal'
+      };
+      var material = new google.charts.Bar(document.getElementById('chart_div'));
+      material.draw(data, options);
+    }
+*/
+    
 	});
 
-})(jQuery, undefined);
+};
 
 
 
@@ -54,40 +94,9 @@ $(function(){
   $button.on('click', function() {
     var value = $input.val();
     $input.val('');
-    console.log(value);
+    getRequest(value);
   });
 });
 
 
-/*
-google.load('visualization', '1', {packages: ['corechart', 'bar']});
-google.setOnLoadCallback(drawMaterial);
-
-function drawMaterial() {
-      var data = google.visualization.arrayToDataTable([
-        ['City', '2010 Population', '2000 Population'],
-        ['New York City, NY', 8175000, 8008000],
-        ['Los Angeles, CA', 3792000, 3694000],
-        ['Chicago, IL', 2695000, 2896000],
-        ['Houston, TX', 2099000, 1953000],
-        ['Philadelphia, PA', 1526000, 1517000]
-      ]);
-
-      var options = {
-        chart: {
-          title: 'Population of Largest U.S. Cities'
-        },
-        hAxis: {
-          title: 'Total Population',
-          minValue: 0,
-        },
-        vAxis: {
-          title: 'City'
-        },
-        bars: 'horizontal'
-      };
-      var material = new google.charts.Bar(document.getElementById('chart_div'));
-      material.draw(data, options);
-    }
-*/
 
