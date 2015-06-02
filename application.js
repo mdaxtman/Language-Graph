f74e53eecac221ecb45b6835767f553789033142
Nick L Olsen <nicholaslolsen@gmail.com>
2015-06-01 20:13:05 -0700

    working on adding a click event

--- application.js
+++ application.js
@@ -1,21 +1,25 @@
 (function($){
 
-	var key = '3ed40dc640c04223adba52fddb235425',
-		capitolWordsAPI = 'http://capitolwords.org/api/1/dates.json',
-		phrase = "mouse",
-		startDate = '1995',
-		granularity = 'years';
 
-	phrase = phrase.replace(/\s/, '+');
+	$('#input').on('click', function(event){
 
-	var requestURI = capitolWordsAPI + '?phrase=' + phrase 
-	+ '&' + 'start_date=' + startDate
-	+ '&' + 'granularity=' + granularity
-	+ '&' + 'apikey=' + key;
+		var phrase = $('#input').val();
+			key = '3ed40dc640c04223adba52fddb235425',
+			capitolWordsAPI = 'http://capitolwords.org/api/1/dates.json',
+			startDate = '1995',
+			granularity = 'years';
 
-	$.get(requestURI, function( data ) {
+		phrase = phrase.replace(/\s/, '+');
 
-		console.log(data.results);
+		var requestURI = capitolWordsAPI + '?phrase=' + phrase 
+							+ '&' + 'start_date=' + startDate
+							+ '&' + 'granularity=' + granularity
+							+ '&' + 'apikey=' + key;
+
+
+		$.get(requestURI, function( data ) {
+			console.log(data.results);
+		});
 
 	});
 
