1c0cc39070cef62bda3b85d5f861518777c9517d
Nick L Olsen <nicholaslolsen@gmail.com>
2015-06-01 20:06:43 -0700

    update

--- application.js
+++ application.js
@@ -2,15 +2,20 @@
 
 	var key = '3ed40dc640c04223adba52fddb235425',
 		capitolWordsAPI = 'http://capitolwords.org/api/1/dates.json',
-		phrase = "United States";
+		phrase = "mouse",
+		startDate = '1995',
+		granularity = 'years';
 
 	phrase = phrase.replace(/\s/, '+');
 
-	var requestURI = capitolWordsAPI + '?phrase=' + phrase + '&' + 'apikey=' + key;
+	var requestURI = capitolWordsAPI + '?phrase=' + phrase 
+	+ '&' + 'start_date=' + startDate
+	+ '&' + 'granularity=' + granularity
+	+ '&' + 'apikey=' + key;
 
 	$.get(requestURI, function( data ) {
 
-		console.log(data);
+		console.log(data.results);
 
 	});
 
